044eb5910fe28e7e0fd624ca2f20011d17fd74ad
Trevor Lee <31684028+trevorwslee@users.noreply.github.com>
2024-03-31 22:06:11 +0800

    updated

--- src/INO/dd_draw.ino
+++ src/INO/dd_draw.ino
@@ -1,46 +1,38 @@
 #include "Arduino_LED_Matrix.h"
 #include "wifidumbdisplay.h"
 
-ArduinoLEDMatrix matrix;
-
+// DumbDisplay using WIFI
 DumbDisplay dumbdisplay(new DDWiFiServerIO(WIFI_SSID, WIFI_PASSWORD));
 
-
-
-
+// will be created in setup()
 LedGridDDLayer *ddMatrix;
 
 
+ArduinoLEDMatrix matrix;
 
+unsigned long frame[] = {
+  0x3184a444,
+  0x42081100,
+  0xa0040000
+};
 
-unsigned long frame[] = { 0, 0, 0 };  // 3 32-bit unsigned longs can holds 96 bits
-
-
-void set_bit(size_t x, size_t y, bool on) {
+void toggle_bit(size_t x, size_t y) {
   size_t bit = x + y * 12;
   int index = bit / 32;
-  int offset = 31 - (bit % 32);
-  if (on) {
-    frame[index] |= (1 << offset);
+  int mask = 1 << (31 - (bit % 32));
+  bool bit_is_on = frame[index] & mask;
+  if (!bit_is_on) {
+    frame[index] |= mask;
   } else {
-    frame[index] &= ~(1 << offset);
+    frame[index] &= ~mask;
   }
 }
-
-int ddData[8] = { 0b100001011111, 
-                  0b100001000100, 
-                  0b100001000100, 
-                  0b111111000100, 
-                  0b100001000100, 
-                  0b100001000100, 
-                  0b100001000100, 
-                  0b100001011111 };
-
 
 
 void setup() {
   Serial.begin(115200);
   matrix.begin();
+  matrix.loadFrame(frame);
 
   ddMatrix = dumbdisplay.createLedGridLayer(12, 8);
   ddMatrix->border(0.3, DD_COLOR_azure);
@@ -48,17 +40,50 @@ void setup() {
   ddMatrix->onColor(DD_COLOR_red);  
   ddMatrix->offColor(DD_COLOR_lightgray);
   ddMatrix->backgroundColor(DD_COLOR_black);
-  //ddMatrix->setFeedbackHandler(FeedbackHandler, "fa");
-  for (int i = 0; i < 8; i++) {
-    int bits = ddData[i];
-    ddMatrix->bitwise(bits, i);
-    for (int j = 0; j < 12; j++) {
-      set_bit(j, i, (bits & (1 << (11 - j))) != 0);
+  ddMatrix->enableFeedback("fa");
+
+  size_t bit = 0;
+  for (int y = 0; y < 8; y++) {
+    int index = bit / 32;
+    int offset = bit % 32;
+    int offset2;
+    if (offset + 12 < 32) {
+      offset2 = -1;
+    } else {
+      offset2 = (bit + 12) % 32;
     }
+    //dumbdisplay.writeComment("index: " + String(index) + " offset: " + String(offset) + " offset2: " + String(offset2));
+    unsigned int bits = frame[index] << offset;
+    if (offset2 != -1) {
+      bits |= frame[index + 1] >> (32 - offset);
+    }
+    //dumbdisplay.writeComment("index: " + String(index) + " offset: " + String(offset) + " offset2: " + String(offset2) + " bits: " + String(bits));
+    ddMatrix->bitwise(bits, y, true);
+    bit += 12;
   }
-  matrix.loadFrame(frame);
+
+  dumbdisplay.writeComment("***");
+  dumbdisplay.writeComment("* long-press to clear");
+  dumbdisplay.writeComment("***");
 }
 
 void loop() {
-
+  const DDFeedback* fb = ddMatrix->getFeedback();
+  if (fb != NULL) {
+    if (fb->type == LONGPRESS) {
+      ddMatrix->clear();
+      for (int i = 0; i < 3; i++) {
+        frame[i] = 0;
+      }
+      matrix.loadFrame(frame);
+    } else {
+      int x = fb->x;
+      int y = fb->y;
+      ddMatrix->toggle(x, y);
+      toggle_bit(x, y);
+      matrix.loadFrame(frame);
+    }
+  }
 }
+
+
