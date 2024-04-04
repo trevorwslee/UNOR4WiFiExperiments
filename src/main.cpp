6809e1f60afc843e11b18ff4455dc060dcd081b0
Trevor Lee <31684028+trevorwslee@users.noreply.github.com>
2024-04-04 17:59:59 +0800

    adding actions to post to dev.to

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,5 @@
 //#include "INO/matrix_test/matrix_test.ino"
 //#include "INO/matrix_obo_test/matrix_obo_test.ino"
 //#include "INO/dd_joystick_test/dd_joystick_test.ino"
-//#include "INO/dd_draw/dd_draw.ino"
-#include "INO/frames/frames.ino"
+#include "INO/dd_draw/dd_draw.ino"
+//#include "INO/frames/frames.ino"
